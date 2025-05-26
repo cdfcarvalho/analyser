3c977b5a2526d2b5940f842d167a88edebd6d213
Célio Carvalho <cdf@estg.ipp.pt>
2025-05-26 20:32:12 +0100

    paul_delete

--- src/main/java/fap/utils/NotEnoughSpaceException.java
+++
@@ -1,6 +0,0 @@
-package src.main.java.fap.utils;
-
-public class NotEnoughSpaceException extends Exception{
-    
-    
-}
--- test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    
-
-    // STRING [100]
-    // int count = 0;
-    // input user
-    // 
-}
